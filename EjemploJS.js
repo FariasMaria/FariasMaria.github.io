38d1a90956d81ac0ee6bc1842e9fc089254de9e7
FariasMaria <126633925+FariasMaria@users.noreply.github.com>
2023-02-28 23:26:22 -0300

    Add files via upload

---
+++ EjemploJS.js
@@ -0,0 +1,5 @@
+
+	document.getElementById('boton').onclick = function () {
+		console.log("Capturamos el evento click");
+		document.getElementById("demo").innerHTML = "Celular: 2215003000 - E-mail: e-mail@gmail.com";
+	}
